d3103b08fc48ca8cbfe71e1f87b58f2685be3e13
Adib-Nur <70833666+Adib-Nur@users.noreply.github.com>
2022-01-30 22:58:26 +0600

    Update financial_report.py

--- program/core/annex_lib/financial_report.py
+++ program/core/annex_lib/financial_report.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from .working_capital_assessment import working_capital
 
 class financial_report:
     def __init__(self, inp_flow):
@@ -10,15 +11,23 @@ class financial_report:
         ]
 
         tot_col = [
-            iflow.purchase_vessel,
-            iflow.furniture,
-            iflow.security_deposit + iflow.adv_payments,
-            iflow.preliminary_exp
+            inp_flow.purchase_vessel,
+            inp_flow.furniture,
+            inp_flow.security_deposit + inp_flow.adv_payments,
+            inp_flow.preliminary_exp
         ]
 
         self.df_project_cost = pd.DataFrame(tot_col, columns=["Total (BDT in Lac)"], index=ixs)
 
-        self.df_project_cost.insert(0, "Equity (BDT in Lac)", df["Total (BDT in Lac)"]*iflow.financial_assumptions.debt_equity["Equity"])
-        self.df_project_cost.insert(1, "Bank-debt (BDT in Lac)", df["Total (BDT in Lac)"]*iflow.financial_assumptions.debt_equity["Debt"])
+        self.df_project_cost.insert(0, "Equity (BDT in Lac)", self.df_project_cost["Total (BDT in Lac)"]*inp_flow.financial_assumptions.debt_equity["Equity"])
+        self.df_project_cost.insert(1, "Bank-debt (BDT in Lac)", self.df_project_cost["Total (BDT in Lac)"]*inp_flow.financial_assumptions.debt_equity["Debt"])
 
-        #self.working_capital_total = 
+        # working capital assessment
+        self.working_capital_total = working_capital(inp_flow).table_main[1].sum()
+        self.working_capital_equity = self.working_capital_total * inp_flow.financial_assumptions.debt_equity["Equity"]
+        self.working_capital_debt = self.working_capital_total * inp_flow.financial_assumptions.debt_equity["Debt"]
+
+        #Total Cost of Project
+        self.total_cost_total = self.df_project_cost["Total (BDT in Lac)"].sum() + self.working_capital_total
+        self.total_cost_equity = self.total_cost_total * inp_flow.financial_assumptions.debt_equity["Equity"]
+        self.total_cost_debt = self.total_cost_total * inp_flow.financial_assumptions.debt_equity["Debt"]
